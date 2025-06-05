e75e46368cec6a78f6009dd9f6bf2c7b05ed9ad8
kraken-wannabe <wireliness@gmail.com>
2025-06-05 19:20:45 -0400

    Initial commit

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,24 @@
+
+        window.alert("Your mama");
+        /*var A = ("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.");*/
+        
+        /*document.write("\"Be who you are and say what you feel,"
+            + " because those who mind don\'t matter and those who matter don\'t mind.\""
+            + "-Dr. Seuss");*/
+
+        /*var B = "Concatenated" + " String"
+        document.write(B)*/
+
+        var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine",
+            Daughter = "Penny", Son = "Zorro";
+        document.write(Son);
+        
+        document.write(3 + 3);
+
+    function My_First_Function() {                      //Defining a function and naming it
+        var str = "This is the button text!";           //Defining a variable and giving it
+                                                        //a string value
+        document.getElementById("Button_Text").innerHTML = str; //Putting the value
+                                                        //of the variable into the HTML elementFromPoint
+                                                        //with the "Button_Text" id
+    }
