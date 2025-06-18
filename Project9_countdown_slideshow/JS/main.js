da385ab3d03f86c7635feca956c5c2895e3da446
kraken-wannabe <wireliness@gmail.com>
2025-06-18 15:28:34 -0400

    Adding Project9 folder for countdown slideshow

---
+++ Project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,31 @@
+function countdown() {
+    var seconds = document.getElementById("seconds").value;
+
+    function tick() {
+        seconds = seconds - 1;
+        timer.innerHTML = seconds;
+        var time = setTimeout(tick, 1000);
+        if (seconds == -1) {
+            alert("Time's up!");
+            clearTimeout(time);
+            timer.innerHTML = "";
+        }
+    }
+    tick();
+}
+//slide index starts at 0
+let slideIndex = 0;
+const slides = document.getElementsByClassName("slide");
+
+function showSlides() {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000); //change image every 3 seconds
+}
+showSlides(); //this line starts the slideshow when the page loads
